8801e721b4266bd5915434fdb5255f56d801a4a7
Uthpala Heenatigala <uthpala@passionatepeople.io>
2025-01-03 17:19:16 +0100

    initial files

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,9 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "React router starter" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
